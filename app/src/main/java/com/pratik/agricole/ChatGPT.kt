d921b73041651cd0b755e9adf65bd4397499a9d7
unknown <i.parthtripathi@gmail.com>
2024-03-07 22:34:44 +0530

    chatpage

---
+++ app/src/main/java/com/pratik/agricole/ChatGPT.kt
@@ -0,0 +1,11 @@
+package com.pratik.agricole
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatGPT : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_gpt)
+    }
+}
