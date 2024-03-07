6679e5411fca1664693088a86a27c98c2432c800
PratikAgrawal02 <pratik.a21@iiits.in>
2024-03-07 21:51:11 +0530

    connected firebase

---
+++ app/src/main/java/com/pratik/agricole/Splash_Screen.kt
@@ -0,0 +1,24 @@
+package com.pratik.agricole
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+
+
+class Splash_screen : AppCompatActivity() {
+    var h: Handler? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        h = Handler()
+        h!!.postDelayed({
+            val auth = FirebaseAuth.getInstance()
+            if (auth.currentUser == null) startActivity(Intent(this@Splash_screen, MainActivity::class.java))//login
+            else {
+                startActivity(Intent(this@Splash_screen, MainActivity::class.java))
+            }
+        }, 3000)
+    }
+}
