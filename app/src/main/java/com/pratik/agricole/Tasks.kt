4e5e0dd1a9e5364f8c451e3626b654006111c6b4
PratikAgrawal02 <pratik.a21@iiits.in>
2024-03-08 21:28:54 +0530

    added todo

--- app/src/main/java/com/pratik/agricole/Tasks.kt
+++ app/src/main/java/com/pratik/agricole/Tasks.kt
@@ -1,11 +1,21 @@
 package com.pratik.agricole
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.pratik.agricole.databinding.FragmentFeildsBinding
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.pratik.agricole.DTO.ToDo
 import com.pratik.agricole.databinding.FragmentTasksBinding
 
 
@@ -13,14 +23,131 @@ class Tasks : Fragment() {
 
     private var _binding: FragmentTasksBinding? = null
     private val binding get() = _binding!!
+    lateinit var dbHandler: DBHandler
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // Inflate the layout for requireActivity() fragment
         _binding =  FragmentTasksBinding.inflate(inflater, container, false)
 
+        dbHandler = DBHandler(requireActivity())
+        binding.rvDashboard.layoutManager = LinearLayoutManager(requireActivity())
+
+        binding.fabDashboard.setOnClickListener {
+            val dialog = AlertDialog.Builder(requireActivity())
+            dialog.setTitle("Add ToDo")
+            val view = layoutInflater.inflate(R.layout.dialog_dashboard, null)
+            val toDoName = view.findViewById<EditText>(R.id.ev_todo)
+            dialog.setView(view)
+            dialog.setPositiveButton("Add") { _: DialogInterface, _: Int ->
+                if (toDoName.text.isNotEmpty()) {
+                    val toDo = ToDo()
+                    toDo.name = toDoName.text.toString()
+                    dbHandler.addToDo(toDo)
+                    refreshList()
+                }
+            }
+            dialog.setNegativeButton("Cancel") { _: DialogInterface, _: Int ->
+
+            }
+            dialog.show()
+        }
         return binding.root
+    }
+
+    fun updateToDo(toDo: ToDo){
+        val dialog = AlertDialog.Builder(requireActivity())
+        dialog.setTitle("Update ToDo")
+        val view = layoutInflater.inflate(R.layout.dialog_dashboard, null)
+        val toDoName = view.findViewById<EditText>(R.id.ev_todo)
+        toDoName.setText(toDo.name)
+        dialog.setView(view)
+        dialog.setPositiveButton("Update") { _: DialogInterface, _: Int ->
+            if (toDoName.text.isNotEmpty()) {
+                toDo.name = toDoName.text.toString()
+                dbHandler.updateToDo(toDo)
+                refreshList()
+            }
+        }
+        dialog.setNegativeButton("Cancel") { _: DialogInterface, _: Int ->
+
+        }
+        dialog.show()
+    }
+
+    override fun onResume() {
+        refreshList()
+        super.onResume()
+    }
+
+
+    private fun refreshList(){
+        binding.rvDashboard.adapter = DashboardAdapter(requireActivity(), this, dbHandler.getToDos())
+    }
+
+
+
+    class DashboardAdapter(val activity1: FragmentActivity, val activity: Tasks, val list: MutableList<ToDo>) :
+        RecyclerView.Adapter<DashboardAdapter.ViewHolder>() {
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+            return ViewHolder(LayoutInflater.from(activity1).inflate(R.layout.rv_child_dashboard, p0, false))
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, p1: Int) {
+            holder.toDoName.text = list[p1].name
+
+            holder.toDoName.setOnClickListener {
+                val intent = Intent(activity1,ItemActivity::class.java)
+                intent.putExtra(INTENT_TODO_ID,list[p1].id)
+                intent.putExtra(INTENT_TODO_NAME,list[p1].name)
+                activity.startActivity(intent)
+            }
+
+            holder.menu.setOnClickListener {
+                val popup = PopupMenu(activity1,holder.menu)
+                popup.inflate(R.menu.dashboard_child)
+                popup.setOnMenuItemClickListener {
+
+                    when(it.itemId){
+                        R.id.menu_edit->{
+                            activity.updateToDo(list[p1])
+                        }
+                        R.id.menu_delete->{
+                            val dialog = AlertDialog.Builder(activity1)
+                            dialog.setTitle("Are you sure")
+                            dialog.setMessage("Do you want to delete this task ?")
+                            dialog.setPositiveButton("Continue") { _: DialogInterface, _: Int ->
+                                activity.dbHandler.deleteToDo(list[p1].id)
+                                activity.refreshList()
+                            }
+                            dialog.setNegativeButton("Cancel") { _: DialogInterface, _: Int ->
+
+                            }
+                            dialog.show()
+                        }
+                        R.id.menu_mark_as_completed->{
+                            activity.dbHandler.updateToDoItemCompletedStatus(list[p1].id,true)
+                        }
+                        R.id.menu_reset->{
+                            activity.dbHandler.updateToDoItemCompletedStatus(list[p1].id,false)
+                        }
+                    }
+
+                    true
+                }
+                popup.show()
+            }
+        }
+
+        class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+            val toDoName: TextView = v.findViewById(R.id.tv_todo_name)
+            val menu : ImageView = v.findViewById(R.id.iv_menu)
+        }
     }
 }
